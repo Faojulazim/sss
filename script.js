3e3f59c53f006649766799c7d53f6a423846e1f8
Epc-Tech <admin@epc-tech.de>
2025-03-02 17:49:25 +0100

    language 1

--- script.js
+++ script.js
@@ -22,15 +22,8 @@ closeIcon.addEventListener("click", (e) => {
   left.classList.add("translate-x-[-150%]");
 });
 
-//Toggle between the PC-Building
-
-//Emaill Integration
-
 //mobile size translate-x-[27px]
 const toggler = document.querySelectorAll("[data-languageSelection]");
-const togglerDiv = document.querySelector("#togglerDiv");
-const circle = document.querySelector("#circle");
-
 toggler.forEach((items) => {
   items.addEventListener("change", () => {
     if (items.value == "Deutsch") {
@@ -40,42 +33,62 @@ toggler.forEach((items) => {
         "Keine Notwendigkeit für überteuerte Sonderanfertigungen. Pro Kunde erstellt, keine versteckten Kosten.";
       document.querySelector("#quoteBtn").innerText =
         "Holen Sie sich ein Angebot";
-      document.querySelector("#galleryHeading").innerText = "Galerie";
-      document.querySelector("#servicesHeading").innerText = "Dienstleistungen";
-      document.querySelector("#pcbuildingText").innerText = "PC Gebäude";
-      document.querySelector("#pcbuildingPageText").innerText = "PC Gebäude";
-      document.querySelector("#repairText").innerText = "Kleine Reparaturen";
-      document.querySelector("#pcbuildingPagePara").innerText =
-        "Sie können Ihren ersten PC nicht bauen? Bauen Sie Ihren ersten PC bei uns und wir kümmern uns um den Rest.";
-      document.querySelector("#emailfirst").placeholder = "Email Adresse";
-      document.querySelector("#priceCatLabel").innerText =
-        "Wählen Sie die Preiskategorie aus";
-      document.querySelector("#customReqText").innerText =
-        "Benutzerdefinierte Anfragen";
-      document.querySelector("#custom").placeholder =
-        "Benutzerdefinierte Anfragen";
-      document.querySelector("#priorExpText").innerText =
-        "Haben Sie bereits Erfahrungen mit PCs?";
+      document.querySelector("#serviceweprovideHeading").innerText =
+        "Dienstleistungen, die wir anbieten";
+      document.querySelector("#ourservices").innerText = "unsere Leistungen";
+      document.querySelector("#servicesPara").innerText =
+        "Wir bieten viele Dienstleistungen an. Sie können den Service auswählen, den Sie benötigen, indem Sie uns kontaktieren. Wir sind seit 2 Jahren im Einsatz.";
+      document.querySelector("#servicesBtn").innerHTML =
+        "Dienstleistungen<i class='fa-solid fa-arrow-right'></i>";
+      document.querySelector("#ourgallery").innerHTML = "Unsere Galerie";
+      document.querySelector("#galleryPara").innerHTML =
+        "Schauen Sie sich unsere Galerie an. Die Galerie zeigt die Projekte, die wir bisher realisiert haben. Unsere RIGs enthalten viele PCs.";
+      document.querySelector("#galleryBtn").innerHTML =
+        "Galerie<i class='fa-solid fa-arrow-right'></i>";
+      document.querySelector("#aboutusText").innerHTML = "Über uns";
+      document.querySelector("#aboutusPara").innerHTML =
+        "Wir sind EPC-Tech. Wir bauen seit 2019 PCs. Bisher haben wir über 3000 Kunden betreut.";
+      document.querySelector("#aboutusBtn").innerHTML =
+        "Über uns<i class='fa-solid fa-arrow-right'></i>";
+      document.querySelector("#contacHeading").innerText =
+        "Kontaktieren Sie uns";
+      document.querySelector("#getintouch").innerText =
+        "Nehmen Sie Kontakt auf";
+      document.querySelector("#contactPara").innerText =
+        "Kontaktieren Sie uns über die unten angegebenen Methoden:";
+      document.querySelector("#messagePlaceholder").innerText = "Nachricht";
+      document.querySelector("#textareaContact").placeholder = "Nachricht";
+      document.querySelector("#contactBtn").innerText = "einreichen";
     } else {
       document.querySelector("#heading").innerHTML =
         "<span class='text-Accent font-[600]'>Custom PC's </span> are on the Horizon";
       document.querySelector("#titleP").innerText =
         "No need for overpriced custom builds. Built per customer, no hidden costs.";
       document.querySelector("#quoteBtn").innerText = "Get a quote";
-      document.querySelector("#galleryHeading").innerText = "Gallery";
-      document.querySelector("#servicesHeading").innerText = "Services";
-      document.querySelector("#pcbuildingText").innerText = "PC-Building";
-      document.querySelector("#repairText").innerText = "Small Repairs";
-      document.querySelector("#pcbuildingPageText").innerText = "PC Building";
-      document.querySelector("#pcbuildingPagePara").innerText =
-        "Unable to build your first PC? Build your first pc from us and we'll take care of the rest.";
-      document.querySelector("#emailfirst").placeholder = "Email Address";
-      document.querySelector("#priceCatLabel").innerText =
-        "Select Price Category";
-      document.querySelector("#customReqText").innerText = "Custom Requests";
-      document.querySelector("#priorExpText").innerText =
-        "Do your have prior experience of PCs?";
-      document.querySelector("#custom").placeholder = "Custom Request";
+      document.querySelector("#serviceweprovideHeading").innerText =
+        "Dienstleistungen, die wir anbieten";
+      document.querySelector("#ourservices").innerText = "Our Services";
+      document.querySelector("#servicesPara").innerText =
+        "We provide a lot of services. You can choose the service you need by contacting us. we have been serving for 2 years.";
+      document.querySelector("#servicesBtn").innerHTML =
+        "Services<i class='fa-solid fa-arrow-right'></i>";
+      document.querySelector("#ourgallery").innerHTML = "Our Gallery";
+      document.querySelector("#galleryPara").innerHTML =
+        "Check out our gallery. The gallery shows the projects we have done so far. Our RIG's contain a lot of PC's.";
+      document.querySelector("#galleryBtn").innerHTML =
+        "Gallery<i class='fa-solid fa-arrow-right'></i>";
+      document.querySelector("#aboutusText").innerHTML = "About Us";
+      document.querySelector("#aboutusPara").innerHTML =
+        "We are EPC-Tech. We have been building PC's since 2019. With over 3000+ customers served till now.";
+      document.querySelector("#aboutusBtn").innerHTML =
+        "About Us<i class='fa-solid fa-arrow-right'></i>";
+      document.querySelector("#contacHeading").innerText = "Contact Us";
+      document.querySelector("#getintouch").innerText = "Get In Touch";
+      document.querySelector("#contactPara").innerText =
+        "Contact us via the methods provided below:";
+      document.querySelector("#messagePlaceholder").innerText = "Message";
+      document.querySelector("#textareaContact").placeholder = "Message";
+      document.querySelector("#contactBtn").innerText = "Submit";
     }
   });
 });
